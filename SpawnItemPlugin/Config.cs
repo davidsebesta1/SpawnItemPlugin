d6f34fab3f9ee3ec2e9d66d0713365b7de1cb8e7
davidsebesta1 <david.sebesta@post.cz>
2023-03-12 12:13:29 +0100

    Add project files.

---
+++ SpawnItemPlugin/Config.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpawnItemPlugin.Plugin
+{
+    internal class Config
+    {
+        [Description("Is plugin enabled")]
+        public bool isEnabled = true; // enabled by default
+
+        [Description("Class ID with random item on spawn")]
+        public int id = 1; // 1 default, d class
+
+        [Description("Item ID to spawn")]
+        public byte itemID = 0; // 0 default, janitor keycard
+    }
+}
---
+++ SpawnItemPlugin/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Enums;
+using PluginAPI.Events;
+
+namespace SpawnItemPlugin.Plugin
+{
+
+    internal class Plugin
+    {
+        [PluginConfig]
+        public Config configuration;
+
+        [PluginEntryPoint("RandomSpawnItems", "1.0.0", "spawn with random item as d class", "davidsebesta")]
+        void Enabled() {
+            EventManager.RegisterEvents((object)this);
+        }
+
+        [PluginEvent(ServerEventType.PlayerSpawn)]
+        void OnPlayerSpawned(Player player, RoleTypeID roleID)
+        {
+
+        }
+    }
+}
